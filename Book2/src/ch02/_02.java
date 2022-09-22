9024c88b12abdaa7921c44505bc2b021972699cc
whynotcry94 <whynotcry94@gmail.com>
2022-09-22 13:49:49 +0900

    2022 09 22

---
+++ Book2/src/ch02/_02.java
@@ -0,0 +1,24 @@
+package ch02;
+
+public class _02 {
+
+	public static void main(String[] args) {
+
+		boolean aBcD;
+		byte 가나다;
+		short _abcd;
+		char $ab_cd;
+		//int 3abcd;
+		long abcd3;
+		//float int;
+		double main;
+		//int my Work;
+		String myClassName;
+		int ABC;
+		
+		final double PI;
+		final int MY_DATA;
+		final float mydata;
+	}
+
+}
---
+++ Book2/src/ch02/_03.java
@@ -0,0 +1,18 @@
+package ch02;
+
+public class _03 {
+
+	public static void main(String[] args) {
+
+		int value1 = 3;
+		{
+			int value2 = 5;
+			System.out.println(value1);
+			System.out.println(value2);
+		}
+		
+		System.out.println(value1);
+		//System.out.println(value2);
+	}
+
+}
